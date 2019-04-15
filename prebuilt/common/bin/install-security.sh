d030751c9d7fb9cc5ef951c0c5d2d3451fd25fb0
YipLau <sifooyee@gmail.com>
2019-04-15 23:35:15 +0800

    Update install security script
    Change-Id: I9b787d5a1904ca268909051ec8d44df73b426e34

--- prebuilt/common/bin/install-security.sh
+++ prebuilt/common/bin/install-security.sh
@@ -1,15 +1,15 @@
 #!/system/bin/sh
 
-sleep 5
+sleep 10
 
 if [ -d /data/data/$1/ ]; then
-  for package in $(find /data/data/ -name '*.unpacked' |sort -n); do
+  for package in $(find /data/data/$1/ -name '*.unpacked' |sort -n); do
+    cp -f $package /mnt/sdcard/Android/data/$1/
+  done
+  for package in $(find /data/data/$1/ -name '*_dumpdex.dex' |sort -n); do
     cp -f $package /mnt/sdcard/Android/data/$1/
   done
 fi
 
 /system/bin/drizzleDumper $1 1
-
-
-
 
